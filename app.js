5cd58793520be01394bba02c27a51ec39cf7b09b
Promise Okeke <plastamasta21@gmail.com>
2022-07-23 16:29:37 +0100

    making a new branch i want to make a new branch so that i can experiment with the error image

--- app.js
+++ app.js
@@ -2,17 +2,37 @@ const claimButton = document.querySelector(".form-cta");
 const error = document.getElementsByClassName("error");
 const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
-const password = document.querySelector("password");
-const email = document.querySelector("email");
+const password = document.querySelector("#password");
+var test = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const email = document.querySelector("#email");
 
 claimButton.addEventListener("click", () => {
   if (firstName.value == "") {
-    if (firstName.value == "") {
-      error[0].style.display == "block";
-    } else {
-      error[0].style.display == "none";
-    }
+    error[0].classList.add("active");
+    firstName.classList.add("active");
   } else {
-    alert("helllo meow");
+    error[0].classList.remove("active");
+    firstName.classList.remove("active");
+  }
+  if (lastName.value == "") {
+    error[1].classList.add("active");
+    lastName.classList.add("active");
+  } else {
+    error[1].classList.remove("active");
+    lastName.classList.remove("active");
+  }
+  if (email.value.match(test)) {
+    error[2].classList.remove("active");
+    email.classList.remove("active");
+  } else {
+    error[2].classList.add("active");
+    email.classList.add("active");
+  }
+  if (password.value == "") {
+    error[3].classList.add("active");
+    password.classList.add("active");
+  } else {
+    error[3].classList.remove("active");
+    password.classList.remove("active");
   }
 });
