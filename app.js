2b0aa26db33c85c251d8e149308b2f98b138b118
Promise Okeke <plastamasta21@gmail.com>
2022-07-23 21:31:02 +0100

    final commit on the test branch I have concluded the project and will now proceed to merge it with the main branch

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const claimButton = document.querySelector(".form-cta");
 const error = document.getElementsByClassName("error");
+const errorImg = document.getElementsByClassName("errorImg");
 const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
 const password = document.querySelector("#password");
@@ -9,30 +10,38 @@ const email = document.querySelector("#email");
 claimButton.addEventListener("click", () => {
   if (firstName.value == "") {
     error[0].classList.add("active");
+    errorImg[0].classList.add("active");
     firstName.classList.add("active");
   } else {
     error[0].classList.remove("active");
+    errorImg[0].classList.remove("active");
     firstName.classList.remove("active");
   }
   if (lastName.value == "") {
     error[1].classList.add("active");
+    errorImg[1].classList.add("active");
     lastName.classList.add("active");
   } else {
     error[1].classList.remove("active");
+    errorImg[1].classList.remove("active");
     lastName.classList.remove("active");
   }
   if (email.value.match(test)) {
     error[2].classList.remove("active");
+    errorImg[2].classList.remove("active");
     email.classList.remove("active");
   } else {
     error[2].classList.add("active");
+    errorImg[2].classList.add("active");
     email.classList.add("active");
   }
   if (password.value == "") {
     error[3].classList.add("active");
+    errorImg[3].classList.add("active");
     password.classList.add("active");
   } else {
     error[3].classList.remove("active");
+    errorImg[3].classList.remove("active");
     password.classList.remove("active");
   }
 });
